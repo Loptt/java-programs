de7261de97c5b2691cc0e0a64af59d80725e1d84
Carlos Estrada <carlos.deg02@hotmail.com>
2018-08-14 23:36:05 -0500

    Finished problem 4

--- src/CTCI_1_4.java
+++ src/CTCI_1_4.java
@@ -3,26 +3,50 @@ import java.util.Scanner;
 
 public class CTCI_1_4  {
 
-    static boolean isPalindrome(String string) {
+    static boolean isBetterPalindrome(String string) {
+
+        string = string.toLowerCase();
 
         char[] arrString = string.toCharArray();
-        Arrays.sort(arrString);
+        short[] values  = new short[128];
+
+        int spaceAmount = 0;
 
         for (int i = 0; i < arrString.length; ++i) {
-            if (arrString[i] == ' ') {
-                char[] shortArrString = Arrays.copyOfRange(arrString, 0, i-1);
+            values[arrString[i]] += 1;
+
+            if (arrString[i] == 0x20) {
+                ++spaceAmount;
             }
         }
 
+        boolean isEven = (arrString.length - spaceAmount) % 2 == 0;
+        boolean middleFound = false;
 
-        for (int i = 0; i < )
+        for (int i = 0; i < values.length; ++i) {
+            if (values[i] % 2 != 0 && i != 0x20) {
+                if (!middleFound && !isEven) {
+                    middleFound = true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
 
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        String string;
+        String string = reader.nextLine();
+
+        if (isBetterPalindrome(string)) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
     }
 }
